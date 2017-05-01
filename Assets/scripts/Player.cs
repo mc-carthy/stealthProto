5abd3503724c5d5fcf8d44fcb26d2bba5ae62619
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-05-01 11:16:49 +0100

    Smooth player input

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -3,16 +3,24 @@
 public class Player : MonoBehaviour {
 
 	private float moveSpeed = 7f;
+    private float smoothMoveTime = 0.1f;
+    private float turnSpeed = 8f;
+    private float smoothInputMagnitude;
+    private float smoothMoveVelocity;
+    private float angle;
 
     private void Update()
     {
         Vector3 inputDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
         float inputMagnitude = inputDirection.magnitude;
 
-        float targetAngle = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg;
-        transform.eulerAngles = Vector3.up * targetAngle;
+        smoothInputMagnitude = Mathf.SmoothDamp(smoothInputMagnitude, inputMagnitude, ref smoothMoveVelocity, smoothMoveTime);
 
-        transform.Translate(transform.forward * moveSpeed * Time.deltaTime * inputMagnitude, Space.World);
+        float targetAngle = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg;
+        angle = Mathf.LerpAngle(angle, targetAngle, Time.deltaTime * turnSpeed);
+        transform.eulerAngles = Vector3.up * angle;
+
+        transform.Translate(transform.forward * moveSpeed * Time.deltaTime * smoothInputMagnitude, Space.World);
     }
 
 }
