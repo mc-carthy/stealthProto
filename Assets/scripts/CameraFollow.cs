ca3f7072e06e91bdb9f183dabfe6eda123fee764
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-05-15 21:28:00 +0100

    Create basic level and guard patrols

---
+++ Assets/scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	private Transform player;
+    private float cameraHeight;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        cameraHeight = transform.position.y;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(player.position.x, cameraHeight, player.position.z);
+    }
+
+}
